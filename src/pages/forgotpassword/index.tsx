eb5fcc318d3d67e6779c468ac6f71509ff318581
unknown <vimalkumarrenu@gmail.com>
2025-09-23 19:34:11 +0530

    deploymentlinkwork

--- src/pages/forgotpassword/index.tsx
+++ src/pages/forgotpassword/index.tsx
@@ -162,6 +162,7 @@ export default function Home() {
                                 onVisibilityChange={setShowConPassword}
                             />
                            
+                           
                             <Button
                                 fullWidth
                                 mt="xl"
